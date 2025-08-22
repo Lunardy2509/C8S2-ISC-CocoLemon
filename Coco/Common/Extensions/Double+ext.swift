68282f08ccc60dde52925e1e0da79d564ec34dd5
Ferdinand Lunardy <ferdilunardy@gmail.com>
2025-08-22 13:34:27 +0700

    Kata Cyntia Harus Rp Not IDR

--- Coco/Common/Extensions/Double+ext.swift
+++ Coco/Common/Extensions/Double+ext.swift
@@ -12,8 +12,8 @@ extension Double {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
         formatter.locale = Locale(identifier: "id_ID")
-        formatter.currencySymbol = "IDR"
+        formatter.currencySymbol = "Rp"
         formatter.maximumFractionDigits = 0
-        return formatter.string(from: NSNumber(value: self)) ?? "IDR 0"
+        return formatter.string(from: NSNumber(value: self)) ?? "Rp0"
     }
 }
